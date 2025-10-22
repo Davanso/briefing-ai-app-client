a940cfb8b70b7f1817ec7064ce662f9a8338661e
davanso <guipvp234@gmail.com>
2025-10-22 20:01:26 -0300

    fix: remove unused import

--- src/pages/private/BriefingsPage.tsx
+++ src/pages/private/BriefingsPage.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { Box, Container, Typography, Card, CardContent, Chip, Button } from '@mui/material';
 import { Add, Edit, Visibility, Delete } from '@mui/icons-material';
 
